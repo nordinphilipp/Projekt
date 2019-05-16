8ca3240aca4858fa3f4200b58474209f1db03cf2
Philip Nordin <philip.nordin.1201@student.uu.se>
2019-05-16 11:07:26 +0200

    Update browse.php

--- browse.php
+++ browse.php
@@ -6,5 +6,5 @@
 	{
 		$query = htmlspecialchars($query); // Ändrar 
 		$query = mysql_real_escape_string($query);
-		$search_name = mysql_query("SELECT * FROM lists WHERE('name' LIKE '%".$query."%')")
+		$search_name = mysql_query("SELECT * FROM lists WHERE('name' LIKE '%".$query."%')"); // Söker efter namn på listor som innehåller sökordet
 	}
