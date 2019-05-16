3407eb3aa304eef16db17dfba509cab1833c907c
Philip Nordin <philip.nordin.1201@student.uu.se>
2019-05-16 11:14:50 +0200

    Update browse.php

--- browse.php
+++ browse.php
@@ -8,3 +8,23 @@
 		$query = mysql_real_escape_string($query);
 		$search_name = mysql_query("SELECT * FROM lists WHERE('name' LIKE '%".$query."%')"); // Söker efter namn på listor som innehåller sökordet
 	}
+	
+	if(mysql_num_rows($search_name) > 0)
+	{
+		while($result = mysql_fetch_array($search_name))
+		{
+                echo "<p>".$result['name']."</p>";
+            }
+             
+        }
+        else
+		{ 
+            echo "Inget hittades";
+        }
+         
+    }
+    else
+	{ 
+        echo "Söktermen måste innehålla minst 3 bokstäver";
+    }
+?>
