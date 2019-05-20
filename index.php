4f2b02318ac103ad193493eec26b6c60224c4c5d
antonstenberg <anton.stenberg@hotmail.com>
2019-05-20 13:50:04 +0200

    Update index.php

--- index.php
+++ index.php
@@ -5,22 +5,17 @@
         <link rel="stylesheet" href="css/main.css">
         <meta charset="utf-8">
         <meta name="viewport" content="width=device-width, initial-scale=1">
-
+        <meta http-equiv="x-ua-compatible" content="ie=edge">
         <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.6.1/css/all.css">
-        <!-- Bootstrap core CSS -->
         <link href="css/bootstrap.min.css" rel="stylesheet">
-        <!-- Material Design Bootstrap -->
         <link href="css/mdb.min.css" rel="stylesheet">
-        <!-- Your custom styles (optional) -->
         <link href="css/style.css" rel="stylesheet">
         <link rel="stylesheet" href="main.css">
         <link href="stylesheet" href="navbar.css">
-
         <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css">
         <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.4.0/jquery.min.js"></script>
         <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js"></script>
         <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js"></script> 
-
     </head>
     <body>
         <?php
@@ -40,20 +35,19 @@
             </div>
         </nav>-->
 
-        <div class="container">
-            <div class="row">
-                
-            </div>
-
-            <div class="row">
-                <div class="col-sm-2 bodyField">
-                    <div class="w-100 p-3 red darken-3" align="center">Popular</div>
+         <div class="container-fluid">
+            <div class="row flex-grow-1" style="height: calc(90vh - 2px)">
+                <div class="col-2 red lighten-1">
+                    <div class="row">
+                        <div class="w-100 p-3 red darken-3 white-text text-center"><b>Popular</b></div>
+                    </div>
                 </div>
-                <div class="col-sm-8 bodyField">
+                <div class="col-8 red lighten-1"></div>
+                <div class="col-2 red lighten-1">
+                    <div class="row">
+                        <div class="w-100 p-3 red darken-3 white-text text-center"><b>Friend Feed</b></div>
 
-                </div>
-                <div class="col-sm-2 bodyfield">
-                    <div class="w-100 p-3 red darken-3" align="center">Friend Feed</div>
+                    </div>
                 </div>
             </div>
         </div>
