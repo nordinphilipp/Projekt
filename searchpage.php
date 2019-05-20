1cbe34b81a8b6009005abdcfd961d93afb89e3d2
fiddleontheroof <50695424+fiddleontheroof@users.noreply.github.com>
2019-05-20 14:43:47 +0200

    Update searchpage.php

--- searchpage.php
+++ searchpage.php
@@ -8,52 +8,39 @@
   <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js"></script>
   <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js"></script>
   <script src="bootstrap.min.js"></script>
-	<link href="navbar.css" rel="stylesheet" type="text/css" />
+	<link href="css/main.css" rel="stylesheet" type="text/css" />
   </head>
-  <body>
-    <nav class="navbar navbar-light red darken-4" >
-            <a class="navbar-brand nav-link active" href="#">MovieMate</a>
-            <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNavAltMarkup" aria-controls="navbarNavAltMarkup" aria-expanded="false" aria-label="Toggle navigation">
-                <span class="navbar-toggler-icon"></span>
-            </button>
-            <div class="collapse navbar-collapse" id="navbarNavAltMarkup">
-                <div class="navbar-nav">
-                    <a class="nav-item nav-link" href="#">Browse</a>
-                    <a class="nav-item nav-link" href="#">Search</a>
-                    <a class="nav-item nav-link" href="#">Login</a>
-                </div>
-            </div>
-    </nav>
+  <body style="background-color:rgb(79, 19, 6);">
+    <?php
+	include 'navbar.php';
+	?>
 	<div class="row" >
-			<div class="col-12">
-				<form action="" method="GET">
-					<div class="input-group mb-3">
-						<input type="text" class="form-control" name="title" placeholder="Search">
-						<div class="input-group-append">
-							<input type="submit" class="btn btn-success" name="search" value="Go">
-						</div>
+		<div class="col-12">
+			<form action="" method="GET">
+				<div class="input-group mb-3">
+					<input type="text" class="form-control" name="title" placeholder="Search">
+					<div class="input-group-append">
+						<input type="submit" class="btn btn-success" name="search" value="Go">
 					</div>
-				</form>	
-			</div>
+				</div>
+			</form>	
+		</div>
 	</div>
 			<?php
 			if(isset($_GET['search']))
 			{
 			?>
-				<div class="row" style="text-align:center;">
-					<div class="col-11">
-						<div class="row" style="color:grey;background-color:white;">
-							<div class="col-4"><h1>Movies</h1></div>
-							<div class="col-4"><h1>Users</h1></div>
-							<div class="col-4"><h1>Lists</h1></div>
-						</div>			
+	
 					<div class="row">
-						<div class="col-4">
+					<div class="col-4"><h1 class="titles">Titles</h1></div>
+					</div>
+					<div class="row" style="padding:10px;">
+						<div class="col-12">
 							<?php
 	
 							$hold = $_GET['title'];
 							$title = str_replace(" ", "+",$hold);
-							$content = file_get_contents("https://www.omdbapi.com/?s=$title&apikey=2c66b43f");
+							$content = file_get_contents("https://www.omdbapi.com/?s=$title&type=movie&apikey=2c66b43f");
 							$arr = json_decode($content);
 							
 							if($arr -> Response == "False" )
@@ -76,13 +63,13 @@
 								{
 								?>
 								<div class="card" style="min-width:200px;max-width:200px;float:left;">
-									<img class="card-img-top" src="<?php echo $test->Poster?>" alt="Card Image"/>
+									<img class="card-img-top" src="<?php echo $test->Poster?>" alt="Card Image" style="min-height:300px;"/>
 									<div class="card-body">
 										<p class="card-text"><?php echo $test->Title?></p>
 										<p class="card-text"><?php echo $test->Year ?></p>
 										<form action="moviepage.php" method="GET">
 											<input type="hidden" name="id" value="<?php echo $test -> imdbID?>" style="visibility:hidden;">	
-											<input type="submit" name="submit" class="btn btn-success" value="See Movie" >
+											<input type="submit" name="submit" class="btn btn-dark" value="See Movie">
 										</form>	
 									</div>
 								</div>	
@@ -91,7 +78,20 @@
 								?>
 							</div>
 						</div>
-						<div class="col-4">
+						</div>
+						
+						
+						<div class="row">
+						<div class="col-4"><h1 class="titles">Users</h1></div>
+						</div>
+						<div class="row" style="padding:10px;">
+						<div class="col-12">
+						<?php
+						//connect to database
+						//get all users where the search is included in their username
+						//print them out in cards in a loop
+						
+						?>
 						<div class="card-deck">
 								<div class="card" style="min-width:200px;max-width:200px;float:left;">
 									<img class="card-img-top" src="" alt="Card Image"/>
@@ -99,12 +99,20 @@
 										<p class="card-text">Username</p>
 										<form action="#" method="GET">
 											<input type="hidden" name="id" value="<?php //userid ?>" style="visibility:hidden;">	
-											<input type="submit" name="submit" class="btn btn-success" value="See Movie" >
+											<input type="submit" name="submit" class="btn btn-success" value="See User" >
 										</form>	
 									</div>
 								</div>	
 							</div>
 						</div>
+						</div>
+						
+						
+						<div class="row">
+						<div class="col-4"><h1 class="titles">Lists</h1></div>
+						</div>
+
+						<div class="row" style="padding:10px;">
 						<div class="col-4">
 						<div class="card-deck">
 								<div class="card" style="min-width:200px;max-width:200px;float:left;">
@@ -114,17 +122,13 @@
 										<p class="card-text">Main Genre</p>
 										<form action="#" method="GET">
 											<input type="hidden" name="id" value="<?php //userid ?>" style="visibility:hidden;">	
-											<input type="submit" name="submit" class="btn btn-success" value="See Movie" >
+											<input type="submit" name="submit" class="btn btn-success" value="See List" >
 										</form>	
 									</div>
 								</div>	
 							</div>
 						</div>
-						
-						
-					</div>
-					</div>
-				</div>
+						</div>
 	<?php
 								
 							}
@@ -134,3 +138,4 @@
 ?>
   </body>
 </html>
+
