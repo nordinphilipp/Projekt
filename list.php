bbe2196662471592e20d48c1a1a84d54f0902fb5
Stringsnapper <joeme244@gmail.com>
2019-05-29 09:29:19 +0200

    fixade så att listor ligger rätt i views

--- list.php
+++ list.php
@@ -1,25 +1,4 @@
-<!--Detta skulle behöva flyttas till rätt-->
-<!DOCTYPE html>
-<html>
-    <head>
-        <meta charset="utf-8">
-        <meta name="viewport" content="width=device-width, initial-scale=1">
-        <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css">
-        <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.4.0/jquery.min.js"></script>
-        <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js"></script>
-        <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js"></script>
-        <script src="bootstrap.min.js"></script>
-        <link href="css/main.css" rel="stylesheet" type="text/css" />
-    </head>
-    <body>
-        <div class="container">
-            <table>
-                <tr>
-                    <td><img class="miniature" src="https://d1csarkz8obe9u.cloudfront.net/posterpreviews/movie-night-flyer-template-7d7861e3d349b92b655900299d774a11_screen.jpg"></td>
-                    <td><a href="#">movietitle</a></td>
-                    <td><a >rating</a></td>
-                </tr>
-            </table>
-        </div>
-    </body>
-</html>
+<?php
+    include('include/bootstrap.php');
+    include('include/views/list.php');
+?>
