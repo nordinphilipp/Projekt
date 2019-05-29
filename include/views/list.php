777b5848c407ad3204b406b8fab70373176903e3
antonstenberg <anton.stenberg@hotmail.com>
2019-05-29 09:47:01 +0200

    Update list.php

--- include/views/list.php
+++ include/views/list.php
@@ -1,49 +1,51 @@
-<!DOCTYPE html>
 <html>
-    <head>
-        <meta charset="utf-8">
-        <meta name="viewport" content="width=device-width, initial-scale=1">
-        <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css">
-        <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.4.0/jquery.min.js"></script>
-        <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js"></script>
-        <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js"></script>
-        <script src="bootstrap.min.js"></script>
-        <link href="css/main.css" rel="stylesheet" type="text/css" />
-    </head>
     <body>
-        <div class="container">
-            <table>
-                <tr>
-                    <td><img class="miniature" src="https://d1csarkz8obe9u.cloudfront.net/posterpreviews/movie-night-flyer-template-7d7861e3d349b92b655900299d774a11_screen.jpg"></td>
-                    <td><a href="#">movietitle</a></td>
-                    <td><a >rating</a></td>
-                                            <!-- Allt inom detta element ska genereras en gÃ¥ng per film i listan -->
+        <div class="container-fluid">
+            <div class="row flex" style="height: calc(90vh - 2px)">
+                <div class="col-2 red lighten-1">
+                </div>
+                <div class="col-8 red lighten-2">
+                    <div class="row">
+                        <div class="w-100 p-3 text-white text-center"><!-- Hämta in listnamn här! -->En Lista</div>
+                    </div>
+                    <div class="row">
+                        <div class="w-100 p-3 text-white text-center"><!-- Verktygslåda -->test</div>
+                    </div>
+                    <table class="table table-striped table-dark table-bordered">
+                <thead>
+                    <tr>
+                    <th style="width:5%; text-align: center;" scope="col">#</th>
+                    <th style="width:5%; text-align: center;" scope="col"></th>
+                    <th style="width:50%;" scope="col">Title</th>
+                    <th style="width:10%; text-align: center;" scope="col">Year</th>
+                    <th style="width:10%; text-align: center;" scope="col">Runtime</th>
+                    <th style="width:10%; text-align: center;" scope="col">Watched</th>
+                    <th style="width:10%; text-align: center;" scope="col">Rating</th>
+                    </tr>
+                </thead>
+                    <tbody>
+                        <!-- Allt inom detta element ska genereras en gång per film i listan -->
                         <?php
-						$uname = "dbtrain_951";
-						$pass = "pqwkjl";
-						$host = "dbtrain.im.uu.se";
-						$dbname = "dbtrain_951";
-						$connect = new mysqli($host, $uname, $pass, $dbname);
-						$listid = $_GET['listID'];
-
-						$query = "SELECT * from movie_list where listID = '$listid'";
-						$check = $connect->query($query);
-						$result = $check -> fetch_array();		//Behövs det en loop för att plocka ut alla movieID's här?
-						$movieID = $result['movieID'];			//Kan jag plocka ut all information om filmerna med IMDB API:t eller måste jag ta jämföra movieID med movieID i table movies 
-						echo $movieID;
-						//Hämta information här någonstans om namnet på filmerna, samt annat som behövs från IMDB API?
-						
-						
-						//Rating för filmerna nedan.
-						$query2 = "SELECT tumbsUP FROM movies WHERE movieID = '$movieID'";
-						$check = $connect->query($query2);
-						$result = $check -> fetch_array();
-						$tumbsUP = $result['tumbsUP'];
                         //foreach()
 
                         ?>
-                </tr>
+                        <tr style="line-height: 70px;">
+                            <th style="text-align: center;" scope="row"><!--Plats i listan-->1</th>
+                            <td><!-- Poster från API/Placeholder--><img class="miniature" src="https://d1csarkz8obe9u.cloudfront.net/posterpreviews/movie-night-flyer-template-7d7861e3d349b92b655900299d774a11_screen.jpg"></td>
+                            <td>EN FILM</td>
+                            <td style="text-align: center;"><!-- Årtal från API -->2019</td>
+                            <td style="text-align: center;"><!-- Speltid från API -->140 min</td>
+                            <td style="text-align: center;"><!-- Sett eller inte från inloggad användare -->Yes</td>
+                            <td style="text-align: center;"><!-- Listskaparens omdöme -->TMB_DOWN</td>
+                        </tr>
+                    </tbody>
             </table>
+                    </div>
+                </div>
+                <div class="col-2 red lighten-1">
+                </div>
+            </div>
         </div>
+    </div>
     </body>
 </html>
