0a12ad2c121d7e3a940409cf28ad5f1874ade898
fiddleontheroof <50695424+fiddleontheroof@users.noreply.github.com>
2019-05-29 22:17:03 +0200

    Update listeditor.php

--- listeditor.php
+++ listeditor.php
@@ -14,11 +14,9 @@ $title = $res['name'];
   <meta charset="utf-8">
   <meta name="viewport" content="width=device-width, initial-scale=1">
   <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css">
-  <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.4.0/jquery.min.js"></script>
   <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js"></script>
   <script src="https://ajax.aspnetcdn.com/ajax/jQuery/jquery-3.4.0.min.js"></script>
   <link href="css/main.css" rel="stylesheet" type="text/css" />
-  <script src="editor.js"></script>
   <script src='assets/js/editor.js'></script>
   </head>
   <body>
