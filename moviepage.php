e4c812453f359f293603c050b3ea88f43df1e786
fiddleontheroof <50695424+fiddleontheroof@users.noreply.github.com>
2019-05-29 21:27:41 +0200

    Update moviepage.php

--- moviepage.php
+++ moviepage.php
@@ -1,181 +1,285 @@
 <?php
-
-
-// ändra till rätt databas och användare
-
-
+include 'navbar.php';
 $connect = new mysqli('localhost', 'root','','testprojekt');
-$userid = 1;//session variabel
-$movie = $_GET['id'];
-if(isset($_GET['addtolist']))
-{
-	$userid = 1;//session variabel
-	$movie = $_GET['id'];
-	$lists;
-	foreach($_GET['checkbox'] as $value){
-		$lists[] = $value;
-	}
-	
-	
-	
-	foreach($lists as $v){
-		$result = mysqli_query($connect, "SELECT orderinlist FROM listrelation where listID = '$v' ORDER BY orderinlist DESC LIMIT 1");
-		$row = mysqli_fetch_array($result);
-		$length=$row['orderinlist'];
-		if(!is_numeric($length))
-		{
-			$length = 0;
-		}
-		$order = $length + 1;
-		$state  = $connect->prepare("INSERT INTO listrelation(listID,movieID,orderinlist) VALUES(?,?,?)");
-		$state->bind_param('sss',$v,$movie,$order);
-		$state->execute();
-		
-	}
-	
-	header('location:moviepage.php?id='.$movie);
-	
-}
+$listid = 1;
 
-if(isset($_GET['addcomment']))
-{
-	$content = $_GET['comment'];
-	$time = date("Y-m-d h:i");
-	$state  = $connect->prepare("INSERT INTO comment(movieID,content,user) VALUES(?,?,?)");
-	$state->bind_param('sss',$movie,$content,$userid);
-	$state->execute();
-	header('location:moviepage.php?id='.$movie);
-}
+$query = "select name from list where listID = '$listid'";
+$check = $connect->query($query);
+$res = $check -> fetch_array();
+$title = $res['name'];
 
 ?>
-
 <!DOCTYPE html>
 <html lang="en">
   <head>
-      <meta charset="utf-8">
+  <meta charset="utf-8">
   <meta name="viewport" content="width=device-width, initial-scale=1">
   <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css">
   <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.4.0/jquery.min.js"></script>
   <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js"></script>
   <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js"></script>
+  <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.4.0/jquery.min.js"></script>
+  <script src="https://ajax.aspnetcdn.com/ajax/jQuery/jquery-3.4.0.min.js"></script>
   <script src="bootstrap.min.js"></script>
-	<link href="css/main.css" rel="stylesheet" type="text/css" />
+  <link href="css/main.css" rel="stylesheet" type="text/css" />
+  
+  <script>
+  
+  var limit = 2;
+  var counter = 0;
+  var swapcounter = 0;
+  var clickedon = ["",""];
+  var moviearr = ["",""];
+  var idarr = ["",""];
+  var loading = 0;
+  
+function thumbs(x){
+	
+	var element = document.getElementById('thumbs' + x).id;
+	var list = document.getElementById('listid').value;
+	var movie = document.getElementById('movie' + x).value;
 
+	
+	if (document.getElementById(element).src == "https://proxy.duckduckgo.com/iu/?u=https%3A%2F%2Fclipartwork.com%2Fwp-content%2Fuploads%2F2017%2F02%2Fclipart-for-thumbs-up.png&f=1") 
+    {
+		$.ajax({
+		type: 'GET',
+		url: 'rating.php?list='+list+'&rating=down&movie=' + movie,
+	success: function (data) {
+
+		document.getElementById(element).src = "https://proxy.duckduckgo.com/iu/?u=https%3A%2F%2Fsignaturesatori.com%2Fwp-content%2Fuploads%2F2017%2F03%2Fthumbs-down.png&f=1"; 
+ 
+  }
+});
+			
+
+	}
+    else 
+    {
+		$.ajax({
+		type: 'GET',
+		url: 'rating.php?list='+list+'&rating=up&movie=' + movie,
+		success: function (data) {
+		document.getElementById(element).src = "https://proxy.duckduckgo.com/iu/?u=https%3A%2F%2Fclipartwork.com%2Fwp-content%2Fuploads%2F2017%2F02%2Fclipart-for-thumbs-up.png&f=1"; 
+									}
+		});
+     
+	}
+  return false;
+}
+
+function swapitems(x){
+	
+	if(loading == 0)
+	{
+	loading = 1;
+	var list = document.getElementById('listid').value;
+	var movie = document.getElementById('movie' + x).value;
+	swapcounter = swapcounter + 1;
+	
+	
+	
+	if(swapcounter==1)
+	{
+		moviearr[0] = movie;
+		clickedon[0] = document.getElementById(x);
+		clickedon[0].style.backgroundColor = "#a6a6a6";
+		idarr[0] = x;
+	}
+	else if(swapcounter == 2)
+	{
+		
+		moviearr[1] = movie;
+		clickedon[1] = document.getElementById(x);
+		clickedon[1].style.backgroundColor = "#a6a6a6";
+		idarr[1] = x;
+		$.ajax
+		(
+		{
+		type: 'GET',
+		url: 'swap.php?list='+list+'&movie1='+ moviearr[0] +'&movie2=' + moviearr[1],
+		success: function (data) 
+		{
+			
+			
+			var hold = "";
+			hold = document.getElementById('thumbs'+idarr[0]).src;
+			document.getElementById('thumbs'+idarr[0]).src = document.getElementById('thumbs'+idarr[1]).src;
+			document.getElementById('thumbs'+idarr[1]).src = hold;
+			
+			
+			hold = document.getElementById('thumbs'+idarr[0]).id;
+			document.getElementById('thumbs'+idarr[0]).id = document.getElementById('thumbs'+idarr[1]).id;
+			document.getElementById('thumbs'+idarr[1]).id = hold;
+			
+		
+			
+			clickedon[0].style.backgroundColor = "#d9d9d9";
+			clickedon[1].style.backgroundColor = "#d9d9d9";
+			
+			var clonedElement1 = clickedon[0].cloneNode(true);
+			var clonedElement2 = clickedon[1].cloneNode(true);
+			
+			clickedon[1].parentNode.replaceChild(clonedElement1, clickedon[1]);
+			clickedon[0].parentNode.replaceChild(clonedElement2, clickedon[0]);
+			
+		}
+		}
+		);
+		
+		
+		swapcounter = 0;
+	}
+	
+	loading = 0;
+	
+	}
+}
+
+  </script>
   </head>
   <body>
 
-  <?php   
-	include 'navbar.php';
-	$content = file_get_contents("https://www.omdbapi.com/?i=$movie&apikey=2c66b43f");
-	$arr = json_decode($content);
   
-  ?>
-	<div class="card" style="max-width:1000px;margin:auto;background-color:#d9d9d9;">
-	<div class="card-body">
-		<div class="row">
-		<div class="col"><h4 class="card-title"><?php echo $arr->Title ?></h4></div>
-		<div class="col"></div>
-		<div class="col">
-		<button type="button" class="btn btn-primary" data-toggle="modal" data-target="#myModal" style="float:right">
-			Add To A List
-		</button>
+<div class="row">
+	<div class="col-1"></div>
+	<div class="col-5" style="background-color:#d9d9d9;">
 		
-		<div class="modal" id="myModal">
-			<div class="modal-dialog">
-				<div class="modal-content">
+	<div class="row">
+	<div class="col4"> <h1 style="border-bottom:1px solid black;"><?php echo $title ?></h1></div>
+	</div>
+	<div class="row">
+	<?php 
+	$co = 0;
+	$result = mysqli_query($connect, "SELECT * FROM listrelation where listID = '$listid' ORDER BY orderinlist DESC LIMIT 1");
+	$row = mysqli_fetch_array($result);
+	$length=$row['orderinlist'];
+	if(!is_numeric($length))
+	{
+		$length = 0;
+	}
+	$query = "select * from listrelation where listID = '$listid' order by orderinlist asc";//välj inlägg med nyast först
+	$check = $connect->query($query);
+	while($row = $check->fetch_array())//gå igenom alla resultat
+	{
+	$id = $row['movieID'];
+	$co = $co + 1;
+	$thumbs = "thumbs" . $co;
+	$content = file_get_contents("https://www.omdbapi.com/?i=$id&apikey=2c66b43f");
+	$arr = json_decode($content);
+	?>
+		<div class="row" style="width:100%;min-height:100px;padding:20px;">
 
-			<!-- Modal Header -->
-			<div class="modal-header">
-				<h4 class="modal-title">Add To List</h4>
-				<button type="button" class="close" data-dismiss="modal">&times;</button>
+			<div class="col-1">
+			<h2 style="font-size:50px;color:black;"><?php echo $co?></h2>
 			</div>
+			<div class="col-10" id="<?php echo $co ?>" onclick="swapitems(this.id)" style="height:100%;border-right:1px solid black;">
+				<div class="row">
+				
+					<input type="hidden" id="movie<?php echo $co?>" value="<?php echo $arr->imdbID ?>">
+					<input type="hidden" id="listid" value="<?php echo $listid?>" name="list">
+					<div class="col-3" style="height:20vh;">
+						<img src="<?php echo $arr-> Poster ?>"  id="poster<?phpecho $co?>"style="max-height:9.8vh;"/>
+					</div>
+					
+					<div class="col-8">
+						<h4 id="title<?phpecho $co?>"><?php echo $arr-> Title ?></h4>
+						<h5 id="year<?phpecho $co?>"><?php echo $arr-> Year ?></h5>
+					</div>
+				</div>
+			</div>	
+			<div class="col-1">
 
-			<!-- Modal body -->
-			<div class="modal-body">
-			<form action="moviepage.php?id=$id" method="get">
-				<?php
-				$connect = new mysqli('localhost', 'root','','testprojekt');
-				$query = "select * from list where userID = 1";
-				$check = $connect->query($query);
-				$counter = 0;
-				while($row = $check->fetch_array())//gå igenom alla resultat
-				{
-					$counter = $counter + 1;
+					<?php
+					if($row['rating'] == "up"):
 					?>
-						<div class="custom-control custom-checkbox">
-							<input type="hidden" value="<?php echo $id?>" name="id"/>
-							<input type="hidden" value="<?php echo $row['listID']?>" name="listid[]"/>
-							<input type="checkbox" class="custom-control-input" id="customCheck<?php echo $counter?>" name="checkbox[]" value="<?php echo $row['listID']?>">
-							<label class="custom-control-label" for="customCheck<?php echo $counter?>"><p><?php echo $row['name']?></p></label>
-						</div><br>
+						<img src="https://proxy.duckduckgo.com/iu/?u=https%3A%2F%2Fclipartwork.com%2Fwp-content%2Fuploads%2F2017%2F02%2Fclipart-for-thumbs-up.png&f=1" id="<?php echo $thumbs?>" style="height:30px;" onclick="thumbs(<?php echo $co?>)"/>
+
 					
 					<?php
-				}
-				
-				?>
-				 <button type="submit" name="addtolist" class="btn btn-primary">Add</button>
-			</form> 
-			</div>
+					else:?>
+						<img src="https://proxy.duckduckgo.com/iu/?u=https%3A%2F%2Fsignaturesatori.com%2Fwp-content%2Fuploads%2F2017%2F03%2Fthumbs-down.png&f=1" id="<?php echo $thumbs?>" style="height:30px;"onclick="thumbs(<?php echo $co?>)"/>
+					<?php endif;?>
 
-			<!-- Modal footer -->
-			<div class="modal-footer">
-				<button type="button" class="btn btn-danger" data-dismiss="modal">Close</button>
 			</div>
+		</div>	
+	<?php
+	}
+	?>
 
-				</div>
 	</div>
 	</div>
-		</div>
-		</div>
-		
+	<div class="col-1"></div>
+	<div class="col-5">
 		<div class="row">
-		<div class="col"><img src="<?php echo $arr->Poster?>" style="width:200px;"/></div>
-		<div class="col"><h4 class="card-title">Plot</h4><br><p><?php echo $arr->Plot ?></p></div>
-		</div> 
-
-		
-		<div class="row">
-		<div class="col-3"><p class="card-text"><?php echo $arr->Country ?></p></div>
-		<div class="col-3"><h4 class="card-title">Rating</h4><br><p class="card-text"><?php echo $arr->imdbRating ?></p></div>
-		<div class="col-3"><h4 class="card-title">Actors</h4><br><p class="card-text"><?php echo $arr->Actors ?></p></div>
-		<div class="col-3"><h4 class="card-title">Director</h4><br><p class="card-text"><?php echo $arr->Director ?></p></div>
-		</div>
-	
-		
-		<div class="row">	
 			<div class="col-12">
-			<form action="" method="get">
-				<label for="comment">Comment:</label>
-				<input type="hidden" value="<?php echo $movie?>" name="id">
-				<textarea class="form-control"  name="comment" placeholder="insert to database"></textarea>
-				<button type="submit" class="btn btn-primary" name="addcomment">Submit</button>
+				<form action="" method="GET">
+					<div class="input-group mb-3">
+						<input type="text" class="form-control" name="title" placeholder="Add To List">
+						<div class="input-group-append">
+							<input type="submit" class="btn btn-success" name="search" value="Go">
+						</div>
+					</div>
 				</form>
 			</div>
 		</div>
-		
-		<?php 
-		//hämta email eller användarnamn för att visa istället för id
-		$query = "select * from comment where movieID = '$movie' order by timestamp desc";//välj inlägg med nyast först
-		$check = $connect->query($query);
-		while($row = $check->fetch_array())
-		{
-
-		?>
-		
 		<div class="row">
 			<div class="col-12">
-				<div class="media border p-3">
-					<img src="<?php echo $arr->Poster?>" alt="John Doe" class="mr-3 mt-3 rounded-circle" style="width:60px;">
-					<div class="media-body">
-						<h4><?php echo $row['user']?><small><i><?php echo $row['timestamp']?></i></small></h4>
-						<p><?php echo $row['content']?></p>
+				<?php
+				if(isset($_GET['search']))
+				{
+				?>
+				<div class="row" style="padding:10px;">
+					<div class="col">
+						<?php
+						$hold = $_GET['title'];
+						$title = str_replace(" ", "+",$hold);
+						$content = file_get_contents("https://www.omdbapi.com/?s=$title&type=movie&apikey=2c66b43f");
+						$arr = json_decode($content);
+						$count = 0;
+						
+						if($arr -> Response == "False" )
+						{
+						?>
+							<div class="card">
+								<h1 class="card-title">Nothing Found</h1>
+							</div>			
+						<?php
+						}
+						else
+						{	
+						?>
+							<div class="card-deck">
+							<?php
+							foreach($arr -> Search as $test)
+							{
+								$count = $count + 1;
+							?>
+							<div class="card" style="min-width:200px;max-width:250px;float:left;min-height:80px;max-height:100px;">
+								<div class="row">
+									<div class="col">
+									<a href="" class="stretched-link" style="color:black;"><?php echo $test->Title . " " . $test->Year ?> </a>
+									</div>
+									<div class="col">
+									<a href="" class="stretched-link" style="color:black;">
+									<img src="https://proxy.duckduckgo.com/iu/?u=http%3A%2F%2Fpngimg.com%2Fuploads%2Fplus%2Fplus_PNG26.png&f=1" style="height:50px;float:right;"/></a>
+									</div>
+								</div>
+							</div>
+							<?php
+							}
+							?>
+							</div>
+						<?php
+						}
+						?>
 					</div>
 				</div>
+				<?php
+				}
+				?>
 			</div>
 		</div>
-		<?php
-		}
-		?>
 	</div>
 </div>
 </body>
