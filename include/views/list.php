4cb4fb4ce6a5d5b8f53e730b638884f4301205ca
nordinphilipp <50575277+nordinphilipp@users.noreply.github.com>
2019-05-29 09:31:24 +0200

    Update list.php

--- include/views/list.php
+++ include/views/list.php
@@ -17,6 +17,31 @@
                     <td><img class="miniature" src="https://d1csarkz8obe9u.cloudfront.net/posterpreviews/movie-night-flyer-template-7d7861e3d349b92b655900299d774a11_screen.jpg"></td>
                     <td><a href="#">movietitle</a></td>
                     <td><a >rating</a></td>
+                                            <!-- Allt inom detta element ska genereras en gÃ¥ng per film i listan -->
+                        <?php
+						$uname = "dbtrain_951";
+						$pass = "pqwkjl";
+						$host = "dbtrain.im.uu.se";
+						$dbname = "dbtrain_951";
+						$connect = new mysqli($host, $uname, $pass, $dbname);
+						$listid = $_GET['listID'];
+
+						$query = "SELECT * from movie_list where listID = '$listid'";
+						$check = $connect->query($query);
+						$result = $check -> fetch_array();		//Behövs det en loop för att plocka ut alla movieID's här?
+						$movieID = $result['movieID'];			//Kan jag plocka ut all information om filmerna med IMDB API:t eller måste jag ta jämföra movieID med movieID i table movies 
+						echo $movieID;
+						//Hämta information här någonstans om namnet på filmerna, samt annat som behövs från IMDB API?
+						
+						
+						//Rating för filmerna nedan.
+						$query2 = "SELECT tumbsUP FROM movies WHERE movieID = '$movieID'";
+						$check = $connect->query($query2);
+						$result = $check -> fetch_array();
+						$tumbsUP = $result['tumbsUP'];
+                        //foreach()
+
+                        ?>
                 </tr>
             </table>
         </div>
