13e62eb8450d10e2eddbe6103cbbc3729f568cc9
fiddleontheroof <50695424+fiddleontheroof@users.noreply.github.com>
2019-05-30 13:11:30 +0200

    Update rating.php

--- include/methods/rating.php
+++ include/methods/rating.php
@@ -1,11 +1,15 @@
 <?php
 
-$connect = mysqli_connect('localhost', 'root','','testprojekt');
+$uname = "dbtrain_1095";
+$pass = "ldchnm";
+$host = "dbtrain.im.uu.se";
+$dbname = "dbtrain_1095";
+$connect = new mysqli($host, $uname, $pass, $dbname);
 $rating = $_GET['rating'];
 $listid = $_GET['list'];
 $userid = 1;
 $movieid = $_GET['movie'];
-$sql = "update listrelation set rating = '$rating' where listID = '$listid' and movieID = '$movieid'";
+$sql = "update movies set rating = '$rating' where listID = '$listid' and movieID = '$movieid'";
 mysqli_query($connect,$sql);
 
 ?>
