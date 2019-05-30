5c94895bef5492dfa881f4f789b12c7da9c63ee3
fiddleontheroof <50695424+fiddleontheroof@users.noreply.github.com>
2019-05-30 13:10:48 +0200

    Update swap.php

--- include/methods/swap.php
+++ include/methods/swap.php
@@ -1,30 +1,34 @@
 <?php
 
-$connect = mysqli_connect('localhost', 'root','','testprojekt');
+$uname = "dbtrain_1095";
+$pass = "ldchnm";
+$host = "dbtrain.im.uu.se";
+$dbname = "dbtrain_1095";
+$connect = new mysqli($host, $uname, $pass, $dbname);
+
 $listid = $_GET['list'];
-$userid = 1;
 $movieid1 = $_GET['movie1'];
 $movieid2 = $_GET['movie2'];
 
 
-$query = "select orderinlist from listrelation where movieID = '$movieid1' and listID = '$listid'";
+$query = "select orderinlist from movie_list where movieID = '$movieid1' and listID = '$listid'";
 $check = $connect->query($query);
 $o1 = $check -> fetch_array();
 	
-$query2 = "select orderinlist from listrelation where movieID = '$movieid2' and listID = '$listid'";
+$query2 = "select orderinlist from movie_list where movieID = '$movieid2' and listID = '$listid'";
 $check2 = $connect->query($query2);
 $o2 = $check2 -> fetch_array();
 	
 $order1 = $o1['orderinlist'];
 $order2 = $o2['orderinlist'];
 
-$q = "update listrelation set orderinlist = 0 where orderinlist = '$order1' and listID = '$listid'";
+$q = "update movie_list set orderinlist = 0 where orderinlist = '$order1' and listID = '$listid'";
 mysqli_query($connect,$q);
 	
-$q2 = "update listrelation set orderinlist = '$order1' where orderinlist = '$order2' and listID = '$listid'";
+$q2 = "update movie_list set orderinlist = '$order1' where orderinlist = '$order2' and listID = '$listid'";
 mysqli_query($connect,$q2);
 	
-$q3 = "update listrelation set orderinlist = '$order2' where orderinlist = 0 and listID = '$listid'";
+$q3 = "update movie_list set orderinlist = '$order2' where orderinlist = 0 and listID = '$listid'";
 mysqli_query($connect,$q3);
 
 ?>
