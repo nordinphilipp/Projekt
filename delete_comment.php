3f90b3cc2a1f57216dcfd59ce6527d9414dac4aa
nordinphilipp <50575277+nordinphilipp@users.noreply.github.com>
2019-06-03 12:14:26 +0200

    Update delete_comment.php

--- delete_comment.php
+++ delete_comment.php
@@ -1,6 +1,6 @@
 <?php
-/*Redirect till moviepage efter behöver fixas.
-Redirect efter användare klickat på knappen för att radera kommentar till delete_comment.php */
+/*
+Redirect efter anvÃ¤ndare klickat pÃ¥ knappen fÃ¶r att radera kommentar till delete_comment.php */
 
     include('include/bootstrap.php');
 	$uname = "dbtrain_1095";
@@ -8,34 +8,35 @@ Redirect efter användare klickat på knappen för att radera kommentar till delete
 	$host = "dbtrain.im.uu.se";
 	$dbname = "dbtrain_1095";
 	$connect = new mysqli($host, $uname, $pass, $dbname);
-    $commentID = $_GET['id']; // Fixa
+    $commentID = $_GET['id']; 
 	$comment = "SELECT * from comments where commentID='$commentID'";
 	$commentresult = $connect ->query($comment);
 	$res = $commentresult -> fetch_array();
 	$userID = $res['userID'];
+	$movieID = $res['movieID'];
 	
-	/* Fixa felmeddelande ifall användaren ej har behörighet att ändra lista, istället för echo */
+	/* Fixa felmeddelande ifall anvÃ¤ndaren ej har behÃ¶righet att Ã¤ndra lista, istÃ¤llet fÃ¶r echo */
 		if(!empty($_SESSION['logged_in']))
 		{
 			if($_SESSION['userID'] == 27)
 				{
-					$sql = "DELETE FROM comments WHERE commentID='$commentID'";
+					$sql = "DELETE FROM comments WHERE movieID='$movieID'";
 					mysqli_query($connect, $sql);
-					//header("Location: moviepage.php?id=$movieID");
+					header("Location: moviepage.php?id=$movieID");
 				}
 			else if($_SESSION['userID'] == $userID)
 				{
-					$sql = "DELETE FROM comments WHERE commentID='$commentID'";
+					$sql = "DELETE FROM comments WHERE movieID='$movieID'";
 					mysqli_query($connect, $sql);
-					//header("Location: list.php?id=$movieID");
+					header("Location: list.php?id=$movieID");
 				}
 			else
 			{
-				echo "Du har ej behörighet för att ändra/ta bort denna lista.";		// Byt design
+				echo "Du har ej behÃ¶righet fÃ¶r att Ã¤ndra/ta bort denna lista.";		// Byt design
 			}
 		}
 		else
 		{
-			echo "Du har ej behörighet för att ändra/ta bort denna lista.";
+			echo "Du har ej behÃ¶righet fÃ¶r att Ã¤ndra/ta bort denna lista.";
 		}
 ?> 
