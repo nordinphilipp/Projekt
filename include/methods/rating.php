3cbf9d74ae3a2f9d0841dc9ac3aaac496085e58b
fiddleontheroof <50695424+fiddleontheroof@users.noreply.github.com>
2019-06-03 11:18:35 +0200

    Update rating.php

--- include/methods/rating.php
+++ include/methods/rating.php
@@ -1,16 +1,31 @@
 <?php
-//skulle föreslå att allt som har med databas att göra görs via 'include/methods/db.php' 
-//och att anslutningarna till db görs därifrån med 'include/process/connect-process.php'
+
 $uname = "dbtrain_1095";
 $pass = "ldchnm";
 $host = "dbtrain.im.uu.se";
 $dbname = "dbtrain_1095";
 $connect = new mysqli($host, $uname, $pass, $dbname);
-$rating = $_GET['rating'];
-$listid = $_GET['list'];
-$userid = 1;
-$movieid = $_GET['movie'];
-$sql = "update movies set rating = '$rating' where listID = '$listid' and movieID = '$movieid'";
-mysqli_query($connect,$sql);
 
+
+$rating = $_GET['rating'];
+$userid = 24;
+$movieid = $_GET['movie'];
+
+
+$check = $connect->prepare("select * from movies2 where userID = '$userid' and movieID = '$movieid'");
+$check->execute();
+$check->store_result();
+$rows = $check->num_rows;
+$viewings = 1;
+if($rows > 0)
+{
+	$sql = "update movies2 set rating = '$rating' where userID = '$userid' and movieID = '$movieid'";
+	mysqli_query($connect,$sql);
+}
+else
+{
+	$state  = $connect->prepare("INSERT INTO movies2(userID,movieID,rating,viewings) VALUES(?,?,?,?)");
+	$state->bind_param('ssss',$userid,$movieid,$rating,$viewings);
+	$state->execute();
+}
 ?>
