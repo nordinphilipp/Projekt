8ccff97c46d3487db720d11d059690d82aa813bc
Stringsnapper <joeme244@gmail.com>
2019-06-03 14:53:19 +0200

    Update upload_process.php

--- upload_process.php
+++ upload_process.php
@@ -13,27 +13,29 @@ if(isset($_POST['submit'])){
     if($check !== false){;
         $uploadOK = 1;
     } else {
-        $errormsg = "Detta är inte en bild";
+        $errormsg = "Detta är inte en bild. ";
         $uploadOK = 0;
     }
 
     if($imageFileType != "jpg" && $imageFileType != "png" && $imageFileType != "jpeg" && $imageFileType != "gif") {
-        $errormsg = $errormsg . "\nEnbart .jpg, .jpeg, .png and .gif är tillåtet";
+        $errormsg = $errormsg . "Enbart .jpg, .jpeg, .png and .gif är tillåtet. ";
         $uploadOK=0;
     }
 
     if($uploadOK == 0){
-        $errormsg = $errormsg . "\nFilen kunde inte laddas upp";
-        echo "<script>alert('$errormsg')</script>"
+        $errormsg = $errormsg . "Filen kunde inte laddas upp";
+		echo $errormsg;
+		header('Location:profile.php');
     } else {
         if(move_uploaded_file($_FILES['uploadFile']['tmp_name'], $file)) {
-            echo "Upload complete"
+            echo "Upload complete";
             $filedir = $dir . $_FILES['uploadFile']['name'];
 
             changeUserImg($filedir);
             header('Location:profile.php');
         } else {
-            $errormsg = $errormsg . "\nNågonting blev fel";
+            echo "\nNågonting blev fel";
+			header('Location:profile.php');
         }
     }
 }
