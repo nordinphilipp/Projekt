890e5b0681d0f95b79879e69819b3767491aebe3
fiddleontheroof <50695424+fiddleontheroof@users.noreply.github.com>
2019-06-03 15:18:42 +0200

    Update changetitle.php

--- include/methods/changetitle.php
+++ include/methods/changetitle.php
@@ -1,9 +1,14 @@
 <?php
-include('include/process/connect_process.php');
+$user = "dbtrain_1095";
+    $pw = "ldchnm";
+    $host = "dbtrain.im.uu.se";
+    $db = "dbtrain_1095";
+    $connection = new mysqli($host, $user, $pw, $db);
+
 $list = $_GET['list'];
 $title = $_GET['name'];
 
 $sql = "UPDATE lists SET name = '$title' WHERE listID = '$list'";
 $ch = $connection->query($sql);
-
+echo $ch;
 ?>
