58eca5c878d47a33722f6743884e5d9bced64138
fiddleontheroof <50695424+fiddleontheroof@users.noreply.github.com>
2019-06-03 12:26:22 +0200

    Update editorfunctions.php

--- include/methods/editorfunctions.php
+++ include/methods/editorfunctions.php
@@ -1,6 +1,5 @@
 <?php
-//skulle föreslå att allt som har med databas att göra görs via 'include/methods/db.php' 
-//och att anslutningarna till db görs därifrån med 'include/process/connect-process.php'
+
 function conn(){
 $uname = "dbtrain_1095";
 $pass = "ldchnm";
@@ -11,6 +10,52 @@ return new mysqli($host, $uname, $pass, $dbname);
 
 }
 
+function gettitle($listid){
+$connect = conn();
+$query = "select name from lists where listID = '$listid'";
+$check = $connect->query($query);
+$res = $check -> fetch_array();
+	mysqli_close($connect);
+return $res['name'];
+}
+
+function getusername($userid){
+$connect = conn();
+$query = "select username from users where userID = '$userid'";
+$check = $connect->query($query);
+$res = $check -> fetch_array();
+	mysqli_close($connect);
+return $res['username'];
+
+}
+
+function setrating($id){
+$connect = conn();
+$query2 = "SELECT rating FROM movies2 WHERE movieID = '$id'";
+$check2 = $connect->query($query2);
+	if ($check2 ->num_rows === 0){
+			mysqli_close($connect);
+		return "0";
+	}
+	else
+	{
+		while($rowtwo = $check2->fetch_assoc())
+		{		mysqli_close($connect);
+			return $rowtwo['rating'];
+		}
+	}
+}
+
+function orderinlist($id){
+$connect = conn();
+	$result = mysqli_query($connect, "SELECT * FROM movie_list where movieID = '$id'");
+	$row = mysqli_fetch_array($result);
+		mysqli_close($connect);
+	return $row['orderinlist'];
+
+}
+
+
 function returnorder($x){
 	$connect = conn();
 	$result = mysqli_query($connect, "SELECT * FROM movie_list where listID = '$x' ORDER BY orderinlist DESC LIMIT 1");
@@ -20,13 +65,15 @@ function returnorder($x){
 	{
 		$length = 0;
 	}
+		mysqli_close($connect);
 	return $length;
 }
 
 function fetchlist($x){
 	$connect = conn();
-	$query = "select * from movie_list where listID = '$x' order by orderinlist asc";//välj inlägg med nyast först
+	$query = "select * from movie_list where listID = '$x' order by orderinlist asc";
 	$check = $connect->query($query);
+	mysqli_close($connect);
 	return $check;
 }
 ?>
