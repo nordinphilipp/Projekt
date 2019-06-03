4b88d1c09d982e79bd60f989ae290739977a953f
Stringsnapper <joeme244@gmail.com>
2019-06-03 14:14:48 +0200

    Update upload_process.php

--- upload_process.php
+++ upload_process.php
@@ -10,8 +10,7 @@ https://www.w3schools.com/php/php_file_upload.asp-->
 if(isset($_POST['submit'])){
     include('include/methods/db.php');
     $check = getimagesize($_FILES["uploadFile"]["tmp_name"]);
-    if($check !== false){
-        echo "Detta är en bild - " . $check["mime"];
+    if($check !== false){;
         $uploadOK = 1;
     } else {
         echo "Detta är inte en bild";
@@ -27,10 +26,11 @@ if(isset($_POST['submit'])){
         echo "Filen kunde inte laddas upp";
     } else {
         if(move_uploaded_file($_FILES['uploadFile']['tmp_name'], $file)) {
-            echo "Filen " . basename($_FILES['uploadFile']['name']) . " har laddats upp.";
+            echo "Upload complete"
             $filedir = $dir . $_FILES['uploadFile']['name'];
 
             changeUserImg($filedir);
+            header('Location:profile.php');
         } else {
             echo "någonting blev fel";
         }
