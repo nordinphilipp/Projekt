b0879d3c00594ab79bcabdfb78eebace3b216f63
nordinphilipp <50575277+nordinphilipp@users.noreply.github.com>
2019-06-03 12:04:56 +0200

    Radera kommentarer. Fixa:
    Redirect till moviepage efter behöver fixas.
    Redirect efter användare klickat på knappen för att radera kommentar till delete_comment.php

---
+++ delete_comment.php
@@ -0,0 +1,41 @@
+<?php
+/*Redirect till moviepage efter behöver fixas.
+Redirect efter användare klickat på knappen för att radera kommentar till delete_comment.php */
+
+    include('include/bootstrap.php');
+	$uname = "dbtrain_1095";
+	$pass = "ldchnm";
+	$host = "dbtrain.im.uu.se";
+	$dbname = "dbtrain_1095";
+	$connect = new mysqli($host, $uname, $pass, $dbname);
+    $commentID = $_GET['id']; // Fixa
+	$comment = "SELECT * from comments where commentID='$commentID'";
+	$commentresult = $connect ->query($comment);
+	$res = $commentresult -> fetch_array();
+	$userID = $res['userID'];
+	
+	/* Fixa felmeddelande ifall användaren ej har behörighet att ändra lista, istället för echo */
+		if(!empty($_SESSION['logged_in']))
+		{
+			if($_SESSION['userID'] == 27)
+				{
+					$sql = "DELETE FROM comments WHERE commentID='$commentID'";
+					mysqli_query($connect, $sql);
+					//header("Location: moviepage.php?id=$movieID");
+				}
+			else if($_SESSION['userID'] == $userID)
+				{
+					$sql = "DELETE FROM comments WHERE commentID='$commentID'";
+					mysqli_query($connect, $sql);
+					//header("Location: list.php?id=$movieID");
+				}
+			else
+			{
+				echo "Du har ej behörighet för att ändra/ta bort denna lista.";		// Byt design
+			}
+		}
+		else
+		{
+			echo "Du har ej behörighet för att ändra/ta bort denna lista.";
+		}
+?> 
