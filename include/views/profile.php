73092c5a331c439c5253d29441dc137244a57767
antonstenberg <anton.stenberg@hotmail.com>
2019-06-03 16:46:45 +0200

    Update profile.php

--- include/views/profile.php
+++ include/views/profile.php
@@ -1,24 +1,23 @@
-<?php 
-if(!empty($_SESSION['logged_in'])){
-    ?> 
-    <html>
+<?php
+		$userID = $_GET['userID'];
+		$userinfo = fetchUserInfo($userID);
+?>   
+
+   <html>
         <body>
             <div class="container-fluid">
                 <div class="row flex" style="height: calc(90vh - 2px)">
-                    <div class="col-2 red lighten-1">
-                        <div class="row">
-                            <div class="w-100 p-3 red darken-3 white-text text-center"><b>Popular</b></div>
-                        </div>
+                    <div class="col-2">
                     </div>
-                    <div class="col-8 red lighten-1">
+                    <div class="col-8">
                         <div class="row">
-                            <div class="w-100 p-3 red darken-3 white-text text-center"><b><?=$_SESSION['username']?></b></div>
+                            <div class="w-100 p-3 red darken-3 white-text text-center"><b><?=$userinfo['username']?>'s Profile</b></div>
                         </div>
                         <br>
                         <div class="container-fluid white-text" style="border:0px;">
                             <div class="row" style="height: 100%;">
                                 <div class="col">
-                                    <img src=<?= $img ?> alt="Profile picture" class="img-thumbnail" style="height:50%;">
+                                    <img src=<?= $userinfo['img'] ?> alt="Profile picture" class="img-thumbnail" style="height:50%; max-height: 200px; max-width: 200px;">
                                     
                                     <form action="upload_process.php" method="POST" enctype="multipart/form-data">
                                         <label for="uploadFile">Byt profilbild</label><br>
@@ -30,30 +29,41 @@ if(!empty($_SESSION['logged_in'])){
                                 <div class="col">
                                 </div>
                                 <div class="col-sm-3" style="margin-right: 10px;">
-                                    <ul class="list-group black-text movie_list">
-                                        <li class="list-group-item row d-flex align-items-center">
+                                    <ul class="list-group white-text movie_list">
+                                        <li class="list-group-item row d-flex align-items-center bg-dark">
                                             <div class="col-md-12 text-center">Recently Watched</div>
                                         </li>
-                                        <li class="list-group-item row d-flex align-items-center">
-                                            <div class="col-md-10 d-flex">1. Avatar</div>
-                                            <div class="col-md-2 d-flex"><img src="img/thumbsup.png" alt="..." class="img"></div>
-                                        </li>
-                                        <li class="list-group-item row d-flex align-items-center">
-                                            <div class="col-md-10 d-flex">1. Avatar</div>
-                                            <div class="col-md-2 d-flex"><img src="img/thumbsup.png" alt="..." class="img"></div>
-                                        </li>
-                                        <li class="list-group-item row d-flex align-items-center">
-                                            <div class="col-md-10 d-flex">1. Avatar</div>
-                                            <div class="col-md-2 d-flex"><img src="img/thumbsup.png" alt="..." class="img"></div>
-                                        </li>
-                                        <li class="list-group-item row d-flex align-items-center">
-                                            <div class="col-md-10 d-flex">1. Avatar</div>
-                                            <div class="col-md-2 d-flex"><img src="img/thumbsup.png" alt="..." class="img"></div>
-                                        </li>
-                                        <li class="list-group-item row d-flex align-items-center">
-                                            <div class="col-md-10 d-flex">1. Avatar</div>
-                                            <div class="col-md-2 d-flex"><img src="img/thumbsup.png" alt="..." class="img"></div>
-                                        </li>
+                                      <?php
+						$recentmoviestwo = fetchRecentlyWatched($userID);
+					
+						
+						while($row = $recentmoviestwo->fetch_array())//gå igenom alla resultat
+						{
+							$movieID = $row['movieID'];
+							$rating = $row['rating'];
+							$content = file_get_contents("http://www.omdbapi.com/?i=$movieID&apikey=2c66b43f");
+							$arr = json_decode($content);
+							?>
+                                    <li class="list-group-item row d-flex align-items-center bg-dark">
+                                        <div class="col-md-10 d-flex"><?php echo $arr -> Title?></div>
+										<?php
+										if($rating == "1"):
+										?>
+										<div class="col-md-2 d-flex"><img src="assets/img/ratings/thumbs_up.png" alt="..." class="img" style="max-height: 30px; max-width: 30px;"></div>
+										<?php
+										elseif($rating == "2"):
+										?>
+										<div class="col-md-2 d-flex"><img src="assets/img/ratings/thumbs_down.png" alt="..." class="img" style="max-height: 30px; max-width: 30px;"></div>
+										<?php
+										else:
+										?>
+										<div class="col-md-2 d-flex"><img src="assets/img/ratings/no_vote.png" alt="..." class="img" style="max-height: 30px; max-width: 30px;"></div>
+										<?php endif;?>
+										<?php
+										}
+										?>
+                            
+                                    </li>
                                     </ul>
                                 </div>
                             </div>
@@ -63,27 +73,22 @@ if(!empty($_SESSION['logged_in'])){
 
                         <div class=col-12>
                             Recent Lists<span style="float: right">View All</span>
-                            <div class=row d-flex align-items-center style="padding-top: 10px;">
-                                <div class=col-12 d-flex style="border: 1px solid black; background-color: gray; height: 11vh;">1. Halloween 2019<span style="float: right">View</span></div>
-                            </div>
-                            <div class=row>
-                                <div class=col-12 d-flex style="border: 1px solid black; background-color: gray; height: 11vh;">2. Bakisfilmer<span style="float: right">View</span></div>
-                            </div>
-                            <div class=row>
-                                <div class=col-12 d-flex style="border: 1px solid black; background-color: gray; height: 11vh;">3. Favoritdokumentärer<span style="float: right">View</span></div>
-                            </div>
-                        </div>
-                    </div>
-                    <div class="col-2 red lighten-1">
-                        <div class="row">
-                            <div class="w-100 p-3 red darken-3 white-text text-center"><b>Friend Feed</b></div>
-                        </div>
-                    </div>
+                            <div class=row d-flex align-items-center style="padding-top: 10px; line-height: 60px;">
+                               <?php
+						$recentlists = fetchRecentLists($userID);
+						
+						while($row = $recentlists->fetch_array())//gå igenom alla resultat
+						{
+							$listnameloop = $row['name'];
+							$idloop = $row['listID'];
+							$link = "list.php?listID=$idloop";
+							?>
+                    <div class=col-12 d-flex style="border: 1px solid black; background-color: #333; height: 60px; color: white;"><?php echo $listnameloop?><span style="float: right"><a href="<?php echo $link?>">View</a></span></div>
+						<?php
+						}
+						?>
+                    
                 </div>
             </div>
         </body>
     </html>
-<?php }else{
-    echo 'Du måste logga in för att se din användarprofil';
-    header('Location: login.php');
-} ?>
